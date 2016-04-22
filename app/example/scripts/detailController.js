cf7ec5fa5a164216180ff35f51695b2533c35c97
slj889 <lal.s2012@gmail.com>
2016-04-21 21:20:09 -0500

    style changes

--- app/example/scripts/detailController.js
+++ app/example/scripts/detailController.js
@@ -4,7 +4,6 @@ angular
     $scope.payload = steroids.view.params.payload;
 
     function convert() {
-
       var payload = $scope.payload;
 
       payload = payload.replace("%20", " ");
@@ -12,33 +11,15 @@ angular
 
       var payloadObj = {};
 
-      // supersonic.logger.log(payload);
-
       for (var i = 0; i < payload.length; i++) {
-
         var key = payload[i].split(":")[0];
         var val = payload[i].split(":")[1];
-
-        supersonic.logger.log(key);
-        supersonic.logger.log(val);
-
         payloadObj[key] = val;
       }
-
-      supersonic.logger.log(payloadObj);
-
       $scope.sanitizedPayload = payloadObj;
-
     }
-
-
+    
     $scope.$watch('payload', function(newValue, oldValue) {
       convert();
-      // if (newValue != oldValue) {
-      //   convert();
-      // }
     });
-
-
-
   });
