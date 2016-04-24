47c5af5406b57cc85964c1092bf29537f41991a5
IvyZX <ivyzhengx@gmail.com>
2016-04-24 14:08:58 -0500

    review worked

--- app/example/scripts/detailController.js
+++ app/example/scripts/detailController.js
@@ -37,16 +37,20 @@ angular
     $scope.giveReviews = function() {
       supersonic.logger.log("submit button is clicked!");
 
+      var index = $scope.sanitizedPayload.bathroomId;
+      //index = string(index);
+      index = parseInt(index);
+      supersonic.logger.log("looking at bathroom " + index);
+
       var deferred = $q.defer();
       var ref = new Firebase('https://scorching-fire-6140.firebaseio.com/');
-      //var brref = ref.child(index.toString());
-      var reviewref = ref.child("reviews");
+      var brref = ref.child(index.toString());
+      var reviewref = brref.child("reviews");
 
       var review_text = $scope.reviewtext.replace(" ", "%20");
-      $scope.bathroom_id = 3;   // let's just do this for now
 
       var the_review = {
-        name : $scope.bathroom_id,
+        name : index,
         rating : $scope.rating,
         review : review_text
       };
@@ -54,7 +58,7 @@ angular
 
       $scope.submitInfo = "Submitted successfully";
 
-      supersonic.logger.log("Review " + $scope.reviewtext + " submitted for " + $scope.bathroom_id);
+      supersonic.logger.log("Review " + $scope.reviewtext + " submitted for " + index);
 
     };
   });
